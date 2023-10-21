5cc6db3c4f817c3a6a6142b41e8e85be7c91203b
EC2 Default User <ec2-user@ip-172-31-32-173.ap-northeast-1.compute.internal>
2023-10-21 07:47:36 +0000

    フラッシュはまだ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,13 @@
 class BooksController < ApplicationController
     def index
         @books = Book.all
+        @book = Book.new
+        if params[:post] && parabs[:post][:title].blank?
+            @book.errors.add(title, "can't be blank")
+           flash[:error].present?
+           flash,now[:error] = flash[:error]
+        end
+        
     end
     def show
         @book = Book.find(params[:id])
@@ -11,22 +18,35 @@ class BooksController < ApplicationController
     def create
         @book = Book.new(book_params)
        if @book.save
-        redirect_to @book, notice: 'Book was successfully created.'
+        redirect_to book_path(@book.id)
+        flash[:notice] = "Book was successfully created."
+        redirect_to book_path(@book)
+        flash[:error] = "prohibited this book from being saved:"
+
        else
         render 'new'
+        
        end
     end
     def edit
-        @book
+        @book = Book.find(params[:id])
     end
     def update
+        @book = Book.find(params[:id])
         if @book.update(book_params)
-         redirect_to @book, notice: 'Book was successfully updated.'
+         redirect_to book_path(@book.id)
+         flash[:notice] = "Book was successfully updated."
+         flash[:error] = "prohibited this book from being saved:"
+
+
+
         else
          render 'edit'
+         flash[:notice] = "error prohibited this book from being saved:"
         end
     end
     def destroy
+       @book = Book.find(params[:id])
        @book.destroy
        redirect_to books_url, notice: 'Book was successfully destroyed.'
     end
